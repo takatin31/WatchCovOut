aa80b2c4b6a9804958f52c2d41b0779bc7ad4421
USER <ga_hammas@esi.dz>
2020-07-01 14:59:49 +0100

    Fixing create user

--- app/src/main/java/com/example/watchcovout_app/RoleActivity.kt
+++ app/src/main/java/com/example/watchcovout_app/RoleActivity.kt
@@ -12,6 +12,9 @@ import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_card.*
 import kotlinx.android.synthetic.main.activity_role.*
 import kotlinx.android.synthetic.main.activity_role.return_btn
+import org.json.JSONObject
+import java.io.ByteArrayOutputStream
+import java.io.DataOutputStream
 
 class RoleActivity : AppCompatActivity() {
 
@@ -64,18 +67,20 @@ class RoleActivity : AppCompatActivity() {
     }
 
     fun createUser(uid : String, role : String, nid : String, nidUrl : String){
-        //var postURL: String = "${resources.getString(R.string.host)}/api/v0/users/:$uid"
-        var postURL = "https://ptsv2.com/t/i6zey-1593550940/post"
+        var postURL: String = "${resources.getString(R.string.host)}/api/v0/users/$uid"
+        //var postURL = "https://ptsv2.com/t/7a3sq-1593610683/post"
 
         if (role == "PROVIDER"){
             postURL += "?serviceProvider"
         }
 
+        Log.i("userInfo", "postUrl : $postURL")
         Log.i("userInfo", "uid : $uid")
         Log.i("userInfo", "nid : $nid")
         Log.i("userInfo", "nidUrl : $nidUrl")
+
         val request = object : FileUploadRequest(
-            Method.POST,
+            Method.PUT,
             postURL,
             Response.Listener {
                 Log.i("success", "comment posted succefully")
@@ -90,19 +95,26 @@ class RoleActivity : AppCompatActivity() {
                 finish()
             },
             Response.ErrorListener {
-                val err = String(it.networkResponse.data)
+
                 Log.i("error", "error while posting comment")
-                Log.i("error", err)
+                Log.i("error", it.toString())
             }
         ){
-            override fun getParams(): MutableMap<String, String> {
-                var params = HashMap<String, String>()
-                params["nid"] = nid
-                params["nationalCardPicURL"] = nidUrl
 
-                return params
+            override fun getBodyContentType(): String {
+                return "application/json"
             }
 
+            override fun getBody(): ByteArray {
+                val byteArrayOutputStream = ByteArrayOutputStream()
+                val dataOutputStream = DataOutputStream(byteArrayOutputStream)
+                val jsonUser = JSONObject()
+                jsonUser.put("nid", nid)
+                jsonUser.put("nationalCardPicURL", nidUrl)
+                dataOutputStream.writeBytes(jsonUser.toString())
+                return byteArrayOutputStream.toByteArray()
+            }
+/*
             @Throws(AuthFailureError::class)
             override fun getHeaders(): MutableMap<String, String> {
                 val pref = getSharedPreferences(resources.getString(R.string.shared_pref),0)
@@ -110,7 +122,7 @@ class RoleActivity : AppCompatActivity() {
                 val headers = java.util.HashMap<String, String>()
                 headers.put("Authorization", "Bearer $token")
                 return headers
-            }
+            }*/
         }
         Volley.newRequestQueue(this).add(request)
     }
